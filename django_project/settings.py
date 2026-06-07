38674aa63de88d1836f762628f28f9ff174beba7
Sokhna <sdieng2003@gmail.com>
2026-06-07 13:46:06 +0000

    user account

--- django_project/settings.py
+++ django_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "blog",
+    "accounts",
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
+LOGIN_REDIRECTION_URL = "home"
+LOGOUT_REDIRECTION_URL = "home"
--- django_project/urls.py
+++ django_project/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include # new
+from django.urls import path, include
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("blog.urls")), # new
+    path("accounts/", include("django.contrib.auth.urls")), 
+    path("accounts/",include("accounts.urls")),
+    path("", include("blog.urls")),
+
 ]

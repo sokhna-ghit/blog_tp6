6ba005085f2979e8ad2259283a7774a4ac13af9c
Sokhna <sdieng2003@gmail.com>
2026-06-07 10:55:44 +0000

    CRUD

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,24 @@
-from urllib import request
-
-from django.shortcuts import render, get_object_or_404
-
-# Create your views here.
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView , DeleteView
+from django.urls import reverse_lazy
 from .models import Post
-def post_list(request):
-    posts = Post.objects.all()
-    return render(request, 'home.html', {'posts': posts})
 
-def post_detail(request, pk): # new
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, "post_detail.html", {"post": post})      
+class BlogListView(ListView):
+        model = Post
+        template_name = "home.html"
+class BlogDetailView(DetailView):
+        model = Post
+        template_name = "post_detail.html"
+class BlogCreateView(CreateView):
+        model = Post
+        template_name = "post_new.html"
+        fields = ["title", "author", "body"]
+class BlogUpdateView(UpdateView): # new
+        model = Post
+        template_name = "post_edit.html"
+        fields = ["title", "body"]
+
+class BlogDeleteView(DeleteView): # new
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy("home")
